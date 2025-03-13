fd8df85abd0164fbeac4ef37b5e3b058428d118e
sSebster <sknet@live.fr>
2025-03-13 17:44:08 +0100

    P3 jusqu'à light en dégradé de blanc

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -2,9 +2,28 @@
 
 out vec4 out_color;
 in vec3 vertex_position;
+in vec3 vertex_normale;
+//uniform vec3 light_direction;
 
 void main()
 {
-    out_color = vec4(1.,1.,0.,1.);
-    out_color = vec4(vertex_position, 1.);
+    vec3 light_direction = vec3(0.,0.,-1.);
+    float light_intensity = max(-dot(vertex_normale,light_direction), 0.);
+    out_color = vec4(vec3(light_intensity), 1.);
+
+
+
+
+ // out_color = vec4(1.,1.,0.,1.);
+ // out_color = vec4(vertex_position, 1.);
+
+ // out_color = vec4(0.25,0.75,0.25,0.5); //bateau fantome hehehehehehehe
+ // out_color = vec4(vertex_normale, 1.);
+
+ // //const vec3 light_direction = normalize(vec3(0.2, 0.3, -1.));
+
+ // //out_color = vec4(vec3(0.5,0.5,0.5)*(dot(vertex_normale,light_direction)+0.3), 1.);
+ // out_color = vec4(0.9,0.,0.,1.);
+
+ // out_color = out_color*dot(out_color,light_direction);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,53 @@
 
 #include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
 
+auto load_mesh(std::filesystem::path const& path) -> gl::Mesh
+{
+    // On lit le fichier avec tinyobj
+    auto reader = tinyobj::ObjReader{};
+    reader.ParseFromFile(gl::make_absolute_path(path).string(), {});
+
+    if (!reader.Error().empty())
+        throw std::runtime_error("Failed to read 3D model:\n" + reader.Error());
+    if (!reader.Warning().empty())
+        std::cout << "Warning while reading 3D model:\n" + reader.Warning();
+
+    // On met tous les attributs dans un tableau
+    auto vertices = std::vector<float>{};
+    for (auto const& shape : reader.GetShapes())
+    {
+        for (auto const& idx : shape.mesh.indices)
+        {
+            // Position
+            vertices.push_back(reader.GetAttrib().vertices[3 * idx.vertex_index + 0]);
+            vertices.push_back(reader.GetAttrib().vertices[3 * idx.vertex_index + 2]);
+            vertices.push_back(reader.GetAttrib().vertices[3 * idx.vertex_index + 1]);
+
+            // UV
+            vertices.push_back(reader.GetAttrib().texcoords[2 * idx.texcoord_index + 0]);
+            vertices.push_back(reader.GetAttrib().texcoords[2 * idx.texcoord_index + 1]);
+
+            // Normale
+            vertices.push_back(reader.GetAttrib().normals[3 * idx.normal_index + 0]);
+            vertices.push_back(reader.GetAttrib().normals[3 * idx.normal_index + 2]);
+            vertices.push_back(reader.GetAttrib().normals[3 * idx.normal_index + 1]);
+        };
+    }
+
+    // TODO créer et return un gl::mesh, qui utilisera le tableau `vertices` en tant que `data` pour son vertex buffer.
+    // Attention, il faudra bien spécifier le layout pour qu'il corresponde à l'ordre des attributs dans le tableau `vertices`.
+    auto mesh_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position3D{0}, gl::VertexAttribute::UV{1}, gl::VertexAttribute::Normal3D{2}},
+            .data   = {vertices
+            },
+        }},
+        },
+    };
+    return mesh_mesh;
+}
+
+
 int main()
 {
     //Debug
@@ -21,12 +68,13 @@ int main()
     //debug
 
     // Initialisation
-    gl::init("Rendering couscous"); // On crée une fenêtre et on choisit son nom
+    gl::init("TP Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    glEnable(GL_BLEND);
-    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_ONE); // On peut configurer l'équation qui mélange deux couleurs, comme pour faire différents blend mode dans Photoshop. Cette équation-ci donne le blending "normal" entre pixels transparents.
+   // glEnable(GL_BLEND);
+  //  glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_ONE); // On peut configurer l'équation qui mélange deux couleurs, comme pour faire différents blend mode dans Photoshop. Cette équation-ci donne le blending "normal" entre pixels transparents.
 
+    glEnable(GL_DEPTH_TEST);
     auto camera = gl::Camera{};
     gl::set_events_callbacks({camera.events_callbacks()});
 
@@ -52,8 +100,8 @@ int main()
             .data   = {
                 -0.5f, -0.5f, -0.5, // Position3D du 1er sommet
                 +0.5f, -0.5f, -0.5f // Position3D du 2ème sommet
-                +0.5f, +0.5f, +0.5f// Position3D du 3ème sommet
-                -0.5f, +0.5f, +0.5f  // Position3D du 4ème sommet
+                +0.5f, +0.5f, +0.5f // Position3D du 3ème sommet
+                -0.5f, +0.5f, +0.5f // Position3D du 4ème sommet
             },
         }},
         .index_buffer   = {
@@ -62,18 +110,27 @@ int main()
         },
     }};
 
+    auto const mesh_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position3D{0}},
+            .data   = {
+
+            },
+        }},
+    }};
+
+    auto mesh = load_mesh("res/bato/fourareen.obj");
     // Rendu à chaque frame
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 1.f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
-        glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
 
         shader.bind(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
 
         shader.set_uniform("aspect_ratio", gl::framebuffer_aspect_ratio());
         shader.set_uniform("positionIG", gl::time_in_seconds());
 
-        rectangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
 
         glm::mat4 const view_matrix = camera.view_matrix();
         gl::set_events_callbacks({
@@ -85,12 +142,14 @@ int main()
             },
         });
 
-        glm::mat4 const projection_matrix = glm::infinitePerspective(glm::radians(45.f), gl::framebuffer_aspect_ratio(), 0.001f);
-        glm::mat4 const rotation = glm::rotate(glm::mat4{1.f}, gl::time_in_seconds() /*angle de la rotation*/, glm::vec3{0.f, 0.f, 1.f} /* axe autour duquel on tourne */);
+        glm::mat4 const projection_matrix = glm::infinitePerspective(glm::radians(90.f), gl::framebuffer_aspect_ratio(), 0.001f);
+        glm::mat4 const rotation = glm::rotate(glm::mat4{1.f}, 0.f/*gl::time_in_seconds()*/ /*angle de la rotation*/, glm::vec3{0.f, 0.f, 1.f} /* axe autour duquel on tourne */);
         glm::mat4 const translation = glm::translate(glm::mat4{1.f}, glm::vec3{0.f, 1.f, 0.f} /* déplacement */);
 
-        glm::mat4 const model_matrix = rotation*translation;
+        glm::mat4 const model_matrix = rotation;
         shader.set_uniform("mat",projection_matrix*view_matrix*model_matrix);
-    }
 
+
+        mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
+    }
 }
